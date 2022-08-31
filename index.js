28a35c4cf49da51cd2e55cff1bb4ed9f553c1da4
Reece <Reecesantana@gmail.com>
2022-08-31 10:56:46 -0400

    updated buttons to display properly

--- index.js
+++ index.js
@@ -1,37 +1,40 @@
-let card1 = 10
-let card2 = 10
+// Cards
+let firstCard = 10
+let secondCard = 6
+let newCard = 3
 
-let hand = card1 + card2 
-
+// Card Status
+let cards = [firstCard, secondCard]
+let hand = firstCard + secondCard 
 aliveStatus = ""
-
-
-
 message = ""
+showHand = "You have a " + firstCard + " & a " + secondCard
 
+// Messages
 let messageEl = document.getElementById("message-el")
-
 let handEl = document.getElementById("hand-el")
 
-
-function tap() {
-    console.log("draw")
+// Button toggles
+let tapButton = document.getElementById("tapButton")
+let playButton = document.getElementById("playButton")
+function buttonDisplay() {
+    if(aliveStatus === "alive"){
+        tapButton.style.display = ""
+        playButton.style.display = "none"
+    } else if(aliveStatus === "won") {
+        tapButton.style.display = "none"
+        playButton.style.display = ""
+    } else {
+        playButton.style.display = ""
+        tapButton.style.display = "none"
+    }
 }
 
-
-function startGame() {
-
-
-
-    let buttonDisplay = document.getElementById("playButton")
-    buttonDisplay.style.display = "none"
-
-    // let tapButton = document.getElementById(tapButton)
+function gameState() {
 
 if (hand < 21) {
     message = "Tap or Hold?"
     aliveStatus = "alive"
-    // tapButton.style.display = "block"
 
 } else if(hand === 21){
     message = "You got Blackjack"
@@ -42,9 +45,23 @@ if (hand < 21) {
     aliveStatus = "lost"
 
 }
-    handEl.textContent = "Your hand is a " + card1 + " & a " + card2;
+    handEl.textContent = showHand;
     messageEl.textContent = message
-
+    buttonDisplay()
 }
 
+function startNewGame() {
+    cards = [firstCard, secondCard]
+    hand = firstCard + secondCard
+    showHand = "Cards: " + firstCard + " " + secondCard
+    gameState()
+    console.log(cards)
+}
 
+function tap() {
+    hand += newCard
+    cards.push(newCard)
+    showHand += " " + newCard
+    gameState()
+    console.log(cards)
+}
